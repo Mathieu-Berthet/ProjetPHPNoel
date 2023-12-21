922ffdf04d93a129cc97983754da313ad6178b97
MB <berthetmathieu@free.fr>
2023-12-21 09:03:39 +0100

    Add path for pdf

--- app_config.php
+++ app_config.php
@@ -4,5 +4,7 @@
 
 return [
     'LOG_PATH' => __DIR__ . '/log-files',
-    'DB_PATH' => __DIR__ . '/db.json'
+    'DB_PATH' => __DIR__ . '/db.json',
+    'CERFA_ENTREPRISE_PATH' => __DIR__ . '/PDF/cerfaEntreprise.pdf',
+    'CERFA_PARTICULIER_PATH' => __DIR__ . '/PDF/cerfaParticulier.pdf'
 ];
